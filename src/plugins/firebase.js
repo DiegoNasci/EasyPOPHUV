1e6e63b198c0820cdcf7a859fb4cf18580bcbccc
diegomp100 <diegomp100@gmail.com>
2018-08-16 21:11:58 -0300

    EPH

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,6 +1,11 @@
 import firebase from 'firebase'
 const config = {
-  
+  apiKey: 'AIzaSyClKRsC9GkL8VJwdTCmx2mHDJYFdHmLg8A',
+  authDomain: 'ephuv-ae866.firebaseapp.com',
+  databaseURL: 'https://ephuv-ae866.firebaseio.com',
+  projectId: 'ephuv-ae866',
+  storageBucket: '',
+  messagingSenderId: '939651355743'
 }
 
 export const fireApp = firebase.initializeApp(config)
