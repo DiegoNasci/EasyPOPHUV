0a9a118a47031f90347f628751f1eefc4b4ee007
diegomp100 <diegomp100@gmail.com>
2018-08-16 22:05:21 -0300

    EPH

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -6,20 +6,12 @@
         style="width:30vw;max-width:150px;"
       >
     </p>
-<<<<<<< HEAD
-    <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
-=======
     <p class="text-faded">Desculpe... A página não foi encontrada!<strong>(404)</strong></p>
->>>>>>> deploy/master
     <q-btn
       color="secondary"
       style="width:200px;"
       @click="$router.push('/')"
-<<<<<<< HEAD
-    >Go back</q-btn>
-=======
     >Voltar</q-btn>
->>>>>>> deploy/master
   </div>
 </template>
 
